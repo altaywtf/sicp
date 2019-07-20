c6b8b21f95e97d87b0b5e92079141ea0c2e8586f
Altay Aydemir <altay.aydemir@gmail.com>
2019-07-20 11:53:58 +0200

    update ex-1.22

--- 1/ex-1.22.rkt
+++ 1/ex-1.22.rkt
@@ -1,8 +1,9 @@
 #lang sicp
 
-(define (square n) (* n n))
+; is-prime? with smallest-divisor implementation O(√n)
+(define (square n)
+  (* n n))
 
-; is-prime? by smallest-divisor implementation O(√n)
 (define (smallest-divisor n)
   (find-divisor n 2))
 
@@ -17,8 +18,10 @@
 (define (is-prime? n)
   (= n (smallest-divisor n)))
 
-; Timed-Prime
-(define (timed-prime-test n) (start-prime-test n (runtime)))
+
+; timed-prime-test
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
   (if (is-prime? n)
@@ -31,10 +34,10 @@
   (display elapsed-time)
   (newline))
 
-; search-for-prime -> checks the primality of consecutive odd integers in a specified range
-(define (is-even? n) (= 0 (remainder n 2)))
 
-(define (search-for-prime n) (search-for-primes n 3))
+; search-for-prime -> checks the primality of consecutive odd integers in a specified range
+(define (search-for-prime n)
+  (search-for-primes n 3))
 
 (define (search-for-primes n counter)
   (if (> counter 0)
@@ -43,15 +46,18 @@
           (search-for-primes (+ n 1) counter))
       (display "--- \n")))
 
-; Find the three smallest primes larger than 1000, 10000, 100000, 1000000
+
+; find the three smallest primes larger than 1000, 10000, 100000, 1000000 to check time grows with √n (√10 = 3.16)
 ; (search-for-prime 1000)
 ; (search-for-prime 10000)
 ; (search-for-prime 100000)
-; (search-for-prime 1000000)
-; (search-for-prime 10000000)
 
-(search-for-prime 1000000000)    
-(search-for-prime 10000000000) 
-(search-for-prime 100000000000)  
 (search-for-prime 1000000000000)
+; 1000000000039: elapsed time: 66050
+; 1000000000061: elapsed time: 64856
+; 1000000000063: elapsed time: 79769
+
 (search-for-prime 10000000000000)
+; 10000000000037: elapsed time: 210356
+; 10000000000051: elapsed time: 200050
+; 10000000000099: elapsed time: 224870
