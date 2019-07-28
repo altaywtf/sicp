4a6ab0ab373ae22c2ffc4fe1d95df12247647506
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-07-28 09:59:17 +0200

    add ex-1.43: repeated higher-order-procedure

---
+++ 1/ex-1.43.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+; ex-1.43: 'repeated' higher-order-prodecure
+
+(define (square x) (* x x))
+(define (compose f g) (lambda (x) (f (g x))))
+
+(define (repeated f times)
+  (if (= times 1)
+      f
+      (compose f (repeated f (- times 1)))))
+
+((repeated square 2) 5)
