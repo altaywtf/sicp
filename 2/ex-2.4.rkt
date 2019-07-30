cecc3e7f4d18d52fa76bf11e8fb6dd8036d28e63
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-07-30 19:18:44 +0200

    add ex-2.4: procedural cons implementation

---
+++ 2/ex-2.4.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+; ex-2.4: implementation of cdr according to car example below
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 1 2))
+(cdr (cons 1 2))
