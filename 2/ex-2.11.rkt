34bdc935989ba73af47c0b2bf0ef98214e858bcb
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-07-31 09:07:04 +0200

    add ex-2.11: ben's torture

---
+++ 2/ex-2.11.rkt
@@ -0,0 +1,99 @@
+#lang sicp
+
+; ex-2.11: implement ben's testing by signs of the endpoints solution for mul-interval
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval)
+  (let ((a (car interval)) (b (cdr interval)))
+    (if (< a b) a b)))
+
+(define (upper-bound interval)
+  (let ((a (car interval)) (b (cdr interval)))
+    (if (< a b) b a)))
+
+(define (get-width interval)
+  (abs (/ (- (lower-bound interval) (upper-bound interval)) 2.0)))
+
+(define (add-interval x y)
+  (make-interval
+   (+ (lower-bound x) (lower-bound y))
+   (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval
+   (- (lower-bound x) (upper-bound y))
+   (- (upper-bound x) (lower-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval
+     (min p1 p2 p3 p4)
+     (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (or (= 0 (upper-bound y)) (= 0 (lower-bound y)))
+      (error "Division by 0" y)
+      (mul-interval
+       x (make-interval
+          (/ 1.0 (upper-bound y))
+          (/ 1.0 (lower-bound y))))))
+
+; lol
+(define (mul-interval-ben x y)
+  (define (is-negative? x) (< x 0))
+  (define (is-positive? x) (not (is-negative? x)))
+  
+  (let ((xL (lower-bound x)) (xU (upper-bound x))
+        (yL (lower-bound y)) (yU (upper-bound y)))
+
+    (cond
+      ; + + | + +
+      ((and (is-positive? xL) (is-positive? xU) (is-positive? yL) (is-positive? yU))
+       (make-interval (* xL yL) (* xU yU)))
+
+      ; + + | - +
+      ((and (is-positive? xL) (is-positive? xU) (is-negative? yL) (is-positive? yU))
+       (make-interval (* xU yL) (* xU yU)))
+
+      ; + + | - -
+      ((and (is-positive? xL) (is-positive? xU) (is-negative? yL) (is-negative? yU))
+       (make-interval (* xU yL) (* xL yU)))
+
+      ; - + | + +
+      ((and (is-negative? xL) (is-positive? xU) (is-positive? yL) (is-positive? yU))
+       (make-interval (* xL yU) (* xU yU)))
+
+      ; - + | - +
+      ((and (is-negative? xL) (is-positive? xU) (is-negative? yL) (is-positive? yU))
+       (make-interval
+        (min (* xL yU) (* yL xU))
+        (max (* xU yU) (* xL yL))))
+
+      ; - + | - -
+      ((and (is-positive? xL) (is-negative? xU) (is-positive? yL) (is-positive? yU))
+       (make-interval (* xU yL) (* xL yL)))
+     
+      ; - - | + +
+      ((and (is-negative? xL) (is-negative? xU) (is-positive? yL) (is-positive? yU))
+       (make-interval (* xL yU) (* xU yL)))
+
+      ; - - | - +
+      ((and (is-negative? xL) (is-negative? xU) (is-negative? yL) (is-positive? yU))
+       (make-interval (* xL yU) (* xL yL)))
+
+      ; - - | - -
+      ((and (is-negative? xL) (is-negative? xU) (is-negative? yL) (is-negative? yU))
+       (make-interval (* xU yU) (* xL yL)))
+
+      (else (error ":D"))
+      )))
+
+; test
+(define i1 (make-interval -5 155))
+(define i2 (make-interval -10 0))
+(mul-interval i1 i2)
+(mul-interval-ben i1 i2)
