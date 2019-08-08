b144b250daaa58236b7b9ad5b56767e55387c5cb
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-08-08 07:52:57 +0200

    add ex-2.36: accumulate-n for list of sequences

---
+++ 2/ex-2.36.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+; ex-2.36 complete accumulate-n
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+; test
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(accumulate-n + 0 s) ; (22 26 30)
