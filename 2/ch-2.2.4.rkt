4449116fa383caf38d53107ab5abfd9ab9b79dcb
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-08-12 07:56:38 +0200

    polish ch-2.2.4

--- 2/ch-2.2.4.rkt
+++ 2/ch-2.2.4.rkt
@@ -1,23 +1,13 @@
 #lang sicp
 
-; ch-2.2.4 -> example: a picture language
+; ch-2.2.4
 
-; compose from ex-1.42
 (define (compose f g)
   (lambda (x) (f (g x))))
-
-(define (display-wave wave)
-  (newline)
-  (display wave)
-  (newline))
-
-(define wave "w")
-
 
 ; flip
 (define (flip-vert painter) (string-append "f-v-" painter))
 (define (flip-horiz painter) (string-append "f-h-" painter))
-
 
 ; rotate180
 (define (rotate180)
@@ -27,26 +17,23 @@
 ; beside
 (define (beside p1 p2) (string-append "|" p1 "|" p2 "|"))
 
-
 ; below
 (define (below p1 p2) (string-append p1 " / " p2))
 
 
-; right-split
-(define (right-split painter n)
-  (if (= n 0)
-      painter
-      (let ((smaller (right-split painter (- n 1))))
-        (beside painter (below smaller smaller)))))
+; split
+(define (split splitter placer)        
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split splitter placer) painter (- n 1))))
+          (splitter painter (placer smaller smaller))))))
 
+; right-split
+(define right-split (split beside below))
 
 ; up-split
-(define (up-split painter n)
-  (if (= n 0)
-      painter
-      (let ((smaller (up-split painter (- n 1))))
-        (below (beside smaller smaller) painter))))
-
+(define up-split (split below beside))
 
 ; corner-split
 (define (corner-split painter n)
@@ -61,31 +48,19 @@
            (below corner bottom-right))))))
 
 
-; higher-order-procedure for abstracting procedures like square-limit, corner-split, flipped pairs
+; higher-order-procedure for abstracting square-limit and flipped pairs
 (define (square-of-four top-left top-right bottom-left bottom-right)
   (lambda (painter)
     (let ((top (beside (top-left painter) (top-right painter)))
           (bottom (beside (bottom-left painter) (bottom-right painter))))
       (below top bottom))))
 
-#|
-(define (flipped-pairs painter)
-  (let ((flipped-painter (beside painter (flip-vert painter))))
-    (below flipped-painter flipped-painter)))
-|#
 
 (define (flipped-pairs painter)
   (let ((combine4 (square-of-four identity flip-vert identity flip-vert)))
     (combine4 painter)))
 
-#|
-(define (square-limit painter n)
-  (let ((quarter (corner-split painter n)))
-    (let ((half (beside quarter quarter)))
-      (below (flip-vert half) half))))
-|#
 
 (define (square-limit painter n)
   (let ((combine4 (square-of-four flip-horiz identity rotate180 flip-vert)))
     (combine4 (corner-split painter n))))
-
