6f0f33f7b999f0e611a5ed8aef5c45ecc757fe6b
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-08-12 08:22:43 +0200

    add ex-2.46: vector utils

---
+++ 2/ex-2.46.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+; ex-2.46: vector constructor, selectors for (x, y) uses (0, 0) as origin
+; also add, sub, scale procedures
+
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect
+   (+ (xcor-vect v1) (xcor-vect v2))
+   (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect
+   (- (xcor-vect v1) (xcor-vect v2))
+   (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect v1 s)
+  (make-vect
+   (* s (xcor-vect v1))
+   (* s (ycor-vect v1))))
