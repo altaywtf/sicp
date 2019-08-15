36d6a775d0218ff00e4588cf9c294c87641c9867
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-08-15 07:58:19 +0200

    add ch-2.2.4-with-sicp-pict for templating the rest of the exercies

---
+++ 2/ch-2.2.4-with-sicp-pict.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(require sicp-pict)
+
+#|
+ all the methods are actually defined sicp-pict
+ this would make it easier to test the actual output
+ while dealing with the remaining exercises
+|#
+
