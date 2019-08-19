8d3e18f223a30426fdb4628472c2bec857822569
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-08-19 08:40:26 +0200

    ch-2.3.2: improve make-sum and make-product

--- 2/ch-2.3.2.rkt
+++ 2/ch-2.3.2.rkt
@@ -2,6 +2,13 @@
 
 ; ch-2.3.2: symbolic differentiation
 
+; helpers
+(define (=number? exp num)
+  (and
+   (number? exp)
+   (= exp num)))
+
+; variable
 (define (variable? e)
   (symbol? e))
 
@@ -13,7 +20,11 @@
 
 ; sum
 (define (make-sum a1 a2)
-  (list '+ a1 a2))
+  (cond
+    ((=number? a1 0) a2)
+    ((=number? a2 0) a1)
+    ((and (number? a1) (number? a2)) (+ a1 a2))
+    (else (list '+ a1 a2))))
 
 (define (addend sum-exp)
   (cadr sum-exp))
@@ -27,8 +38,13 @@
    (eq? (car x) '+)))
 
 ; product
-(define (make-product a1 a2)
-  (list '* a1 a2))
+(define (make-product m1 m2)
+  (cond
+    ((or (=number? m1 0) (=number? m2 0)) 0)
+    ((=number? m1 1) m2)
+    ((=number? m2 1) m1)
+    ((and (number? m1) (number? m2)) (* m1 m2))
+    (else (list '* m1 m2))))
 
 (define (multiplier pro-exp)
   (cadr pro-exp))
@@ -41,7 +57,6 @@
    (pair? x)
    (eq? (car x) '*)))
 
-
 ; derivative
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -51,8 +66,14 @@
          (make-sum (deriv (addend exp) var)
                    (deriv (augend exp) var)))
         ((product? exp)
-         (make-product (deriv (multiplicand exp) var)
-                       (deriv (multiplier exp) var)))
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
         (else
          (error "unkonwn expression type -- DERIV" exp))))
 
+(deriv '(+ x 3) 'x) ; 1
+(deriv '(* x y) 'x) ; y
+(deriv '(* (* x y) (+ x 3)) 'x) ; (+ (* x y) (* y (+ x 3)))
