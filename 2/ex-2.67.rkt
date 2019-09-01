8e74fc6e6c9e1130e046618b64f6dcff02e02505
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-09-01 09:29:37 +0200

    rename var in ex-2.67

--- 2/ex-2.67.rkt
+++ 2/ex-2.67.rkt
@@ -70,6 +70,6 @@
                    (make-leaf 'B 2)
                    (make-code-tree (make-leaf 'D 1) (make-leaf 'C 1)))))
 
-(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(define sample-bits '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-(decode sample-message sample-tree) ; (A D A B B C A)
+(decode sample-bits sample-tree) ; (A D A B B C A)
