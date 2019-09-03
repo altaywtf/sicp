1e2aa1e853fc994f5581673f360fcec4289860cf
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-09-03 08:02:30 +0200

    update ex-2.68 to import common procedures

--- 2/ex-2.68.rkt
+++ 2/ex-2.68.rkt
@@ -1,67 +1,7 @@
-#lang sicp
+#lang racket
+(require "./ch-2.3.4.rkt")
 
 ; ex-2.68: encode sample message
-
-; leaf {'leaf symbol weight}
-(define (make-leaf symbol weight)
-  (list 'leaf symbol weight))
-
-(define (leaf? object)
-  (eq? (car object) 'leaf))
-
-(define (symbol-leaf leaf)
-  (cadr leaf))
-
-(define (weight-leaf leaf)
-  (caddr leaf))
-
-; code tree -> {left-branch, right-branch, symbols, weight}
-(define (make-code-tree left-branch right-branch)
-  (list
-   left-branch
-   right-branch
-   (append
-    (symbols left-branch)
-    (symbols right-branch))
-   (+
-    (weight left-branch)
-    (weight right-branch))))
-
-; selectors
-(define (left-branch tree)
-  (car tree))
-
-(define (right-branch tree)
-  (cadr tree))
-
-(define (symbols object)
-  (if (leaf? object)
-      (list (symbol-leaf object))
-      (caddr object)))
-
-(define (weight object)
-  (if (leaf? object)
-      (weight-leaf object)
-      (cadddr object)))
-
-; decoding
-(define (decode bits tree)
-  (define (choose-branch bit branch)
-    (cond ((= 0 bit) (left-branch branch))
-          ((= 1 bit) (right-branch branch))
-          (else (error "bad bit: " bit))))
-  
-  (define (decode-1-iter bits current-branch)
-    (if
-     (null? bits) '()
-     (let ((next-branch (choose-branch (car bits) current-branch)))
-       (if
-        (leaf? next-branch)
-        (cons (symbol-leaf next-branch) (decode-1-iter (cdr bits) tree))
-        (decode-1-iter (cdr bits) next-branch)))))
-
-  (decode-1-iter bits tree))
-
 
 ; encoding
 (define (encode-symbol symbol tree)
