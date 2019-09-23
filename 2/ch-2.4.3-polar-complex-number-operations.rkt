8cbfa7f0b06054bd62783506321a9e9c8b23604d
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-09-23 08:39:26 +0200

    modularize rectangular and polar complex-number operations

---
+++ 2/ch-2.4.3-polar-complex-number-operations.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require "./ch-2.4.2-tagged-data.rkt")
+(require "./ch-2.4.3-lookup-table.rkt")
+
+(provide install-polar-package)
+(define (install-polar-package)
+  ;; internal procedures
+  (define (square x) (* x x))
+  
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+  (define (make-from-mag-ang r a)
+    (cons r a))
+
+  ;; provided interface
+  (define (tag x) (attach-tag 'polar x))
+
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+
+  (put 'make-from-mag-ang 'polar
+       (lambda (x y) (tag (make-from-mag-ang x y))))
+
+  'installed-polar-package)
---
+++ 2/ch-2.4.3-rectangular-complex-number-operations.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require "./ch-2.4.2-tagged-data.rkt")
+(require "./ch-2.4.3-lookup-table.rkt")
+
+(provide install-rectangular-package)
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (square x) (* x x))
+  
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  
+  (define (make-from-real-imag x y)
+    (cons x y))
+
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  ;; provided interface
+  (define (tag x) (attach-tag 'rectangular x))
+
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (x y) (tag (make-from-mag-ang x y))))
+
+  'installed-rectangular-package)
