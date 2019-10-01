4bfb35446f00577f9c1ed424c27061e9d8fda2ba
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-10-01 08:40:26 +0200

    add coercion-lookup-table and apply-generic-with-coercion for ch-2.5.2

---
+++ 2/ch-2.5.2-apply-generic-with-coercion.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(require "./ch-2.4.2-tagged-data.rkt")
+(require "./ch-2.4.3-lookup-table.rkt")
+(require "./ch-2.5.2-coercion-lookup-table.rkt")
+
+(provide apply-generic)
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cdr type-tags))
+                    (arg1 (car args))
+                    (arg2 (cdr args)))
+
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+
+                  (cond ((t1->t2)
+                         (apply-generic op (t1->t2 arg1) arg2))
+                        ((t2->t1)
+                         (apply-generic op arg2 (t2->t1 arg2)))
+                        (else
+                         (error "No method for these types" (list op type-tags))))))
+
+              (error "No method for these types" (list op type-tags)))))))
