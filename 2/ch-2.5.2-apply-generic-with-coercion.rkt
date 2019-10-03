f3dac877c4aac7edab00b215d715c63f21bee837
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-10-03 08:44:01 +0200

    fix typo in apply-generic-with-coercion

--- 2/ch-2.5.2-apply-generic-with-coercion.rkt
+++ 2/ch-2.5.2-apply-generic-with-coercion.rkt
@@ -7,6 +7,7 @@
 (provide apply-generic)
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
+    
     (let ((proc (get op type-tags)))
       (if proc
           (apply proc (map contents args))
@@ -14,16 +15,16 @@
               (let ((type1 (car type-tags))
                     (type2 (cadr type-tags))
                     (arg1 (car args))
-                    (arg2 (cdr args)))
+                    (arg2 (cadr args)))
 
                 (if (eq? type1 type2)
                     (error "No method defined for this type: " type1)
                     (let ((t1->t2 (get-coercion type1 type2))
                           (t2->t1 (get-coercion type2 type1)))
 
-                      (cond ((t1->t2)
+                      (cond (t1->t2
                              (apply-generic op (t1->t2 arg1) arg2))
-                            ((t2->t1)
+                            (t2->t1
                              (apply-generic op arg2 (t2->t1 arg2)))
                             (else
                              (error "No method for these types: " (list op type-tags)))))))
