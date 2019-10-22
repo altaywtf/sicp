11598e740577ca14ee7dc29321830c8054ab7142
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-10-22 08:13:32 +0200

    export make-account in ex-3.03

--- 3/ex-3.03.rkt
+++ 3/ex-3.03.rkt
@@ -2,6 +2,7 @@
 
 ; ex-3.3: modify make-account with password
 
+(provide make-account)
 (define (make-account balance initial-password)
   (define (withdraw amount)
     (if (>= balance amount)
@@ -28,8 +29,7 @@
   
   dispatch)
 
-(define acc (make-account 100 'secret-password))
-
-((acc 'secret-password 'withdraw) 50)
-
-((acc 'invalid-password 'withdraw) 50)
+; test
+; (define acc (make-account 100 'secret-password))
+; ((acc 'secret-password 'withdraw) 50)
+; ((acc 'invalid-password 'withdraw) 50)
