99b1b46303c0bd1e449cf3e67ee4a2803aa9390a
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-10-24 08:11:32 +0200

    add ex-3.12: append!

---
+++ 3/ex-3.12.rkt
@@ -0,0 +1,33 @@
+#lang sicp
+
+; ex-3.12: mutative append -> append!
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+
+(define z (append x y))
+
+z ; -> (a b c d)
+
+(cdr x) ; -> (b)
+
+; mutates x :(
+(define w (append! x y))
+
+w ; -> (a b c d)
+
+(cdr x) ; -> (b c d)
